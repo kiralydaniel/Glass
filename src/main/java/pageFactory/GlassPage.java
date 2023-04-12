e84faa5ac88f9f9c03720ddc454200dd95bfccfa
nexie12 <nexie.dpl@gmail.com>
2023-04-12 11:26:20 +0200

    GlassWorkflowTest Fetch Table Contents

--- src/main/java/pageFactory/GlassPage.java
+++ src/main/java/pageFactory/GlassPage.java
@@ -1,15 +1,45 @@
 package pageFactory;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import java.net.MalformedURLException;
 
 public class GlassPage extends BasePage{
-    @FindBy(css = "#app-root > div > div > div:nth-child(3) > div:nth-child(4) > div > div > div.transitions-table-holder > div > table > tbody")
+
+    @FindBy(xpath = "//*[@id=\"app-root\"]/div/div/div[3]/div[4]/div/div/div[2]/div/table")
     WebElement table;
 
-    public WebElement returnTable(){
+    @FindBy(css = "#jira > div.atlaskit-portal-container > div:nth-child(2) > div > div:nth-child(3) > div > div > div > div > div > div:nth-child(2) > button > span")
+    WebElement skipButton;
+
+//    WebElement table2 = driver.findElement(By.cssSelector("#app-root > div > div > div:nth-child(3) > div:nth-child(4) > div > div > div.transitions-table-holder > div > table"));
+
+    public List<List> returnTable(){
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        List<List> table = new ArrayList<>();
+
+        for (WebElement row : rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            List<String> rowList = new ArrayList<>();
+            for (WebElement cell : cells) {
+                String text = cell.getText();
+
+                rowList.add(text);
+            }
+            table.add(rowList);
+        }
+        table.remove(0);
         return table;
+    }
+
+    public void clickSkipButton(){
+        wait.until(ExpectedConditions.elementToBeClickable(skipButton));
+        skipButton.click();
     }
 }
--- src/tests/GlassWorkflowTest.java
+++ src/tests/GlassWorkflowTest.java
@@ -1,7 +1,38 @@
+import com.codeborne.selenide.impl.WebElementSelector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pageFactory.DashboardPage;
 import pageFactory.GlassPage;
+import pageFactory.LoginPage;
+import pageFactory.Util;
+
+import java.net.MalformedURLException;
+import java.sql.Driver;
 
 public class GlassWorkflowTest {
     static GlassPage glassPage;
+    static LoginPage loginPage;
+    static DashboardPage dashboardPage;
 
-    System.out.println(glassPage.returnTable());
+    @BeforeEach
+    public void init() throws MalformedURLException, InterruptedException {
+        dashboardPage = new DashboardPage();
+        glassPage = new GlassPage();
+        loginPage = new LoginPage();
+        loginPage.navigateToDashboardLoginPage();
+        loginPage.loggingIn(Util.VALID_USERNAME, Util.VALID_PASSWORD);
+        Thread.sleep(2000);
+        loginPage.navigate("https://jira-expert.codecool.metastage.net/projects/BET?selectedItem=com.metainf.jira.plugin:glass-project-documentation#/home/issueTypes/10004/transitions");
+    }
+
+    @Test
+    public void printTable(){
+        glassPage.clickSkipButton();
+        System.out.println(glassPage.returnTable());
+        System.out.println(glassPage.returnTable().size());
+    }
+
+
 }

c2cc1d38f08b5a5c651346bace54a69acf63921d
SsdHUN <santhasoma1999@gmail.com>
2023-04-12 09:55:41 +0200

    implement: WebDriverFactory

--- src/main/java/pageFactory/WebDriverFactory.java
+++ src/main/java/pageFactory/WebDriverFactory.java
@@ -1,4 +1,40 @@
 package pageFactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+
+
 public class WebDriverFactory {
+    private static WebDriver webDriver = null;
+
+    public static WebDriver createWebDriver(String browserName) {
+        if (webDriver == null) {
+                switch (browserName) {
+                    case "Firefox": {
+                        webDriver = new FirefoxDriver();
+                        break;
+                    }
+                    case "Chrome": {
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.addArguments("--remote-allow-origins=*");
+                        webDriver = new ChromeDriver(chromeOptions);
+                        break;
+                    }
+                }
+            }
+        return webDriver;
+    }
+
+    public static void shutDown() {
+        webDriver.quit();
+        webDriver = null;
+    }
 }

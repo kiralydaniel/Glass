4de3ddafb74005dccc8770068ba8bae9309eaea5
SsdHUN <santhasoma1999@gmail.com>
2023-04-12 10:02:08 +0200

    implement BasePage

--- src/main/java/pageFactory/BasePage.java
+++ src/main/java/pageFactory/BasePage.java
@@ -1,4 +1,25 @@
 package pageFactory;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+
 public class BasePage {
+    WebDriverWait wait;
+    WebDriver driver;
+    String browserType = System.getProperty("browserType");
+
+    public BasePage() {
+        if (browserType == null){
+            browserType = "Chrome";
+        }
+        this.driver = WebDriverFactory.createWebDriver(browserType);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        PageFactory.initElements(driver, this);
+    }
+
+    public void quit(){WebDriverFactory.shutDown(); }
 }

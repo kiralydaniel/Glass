5e3d8aafb9dfa10c8c8078a4c534f723fa254bf0
SsdHUN <santhasoma1999@gmail.com>
2023-04-13 13:43:39 +0200

    fix tabelValidation

--- src/tests/GlassWorkflowTest.java
+++ src/tests/GlassWorkflowTest.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -11,14 +9,16 @@ import pageFactory.LoginPage;
 import pageFactory.Util;
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class GlassWorkflowTest {
     static GlassPage glassPage;
     static LoginPage loginPage;
     static DashboardPage dashboardPage;
+     private static List<List> tabel;
 
-    @BeforeEach
-    public void init() throws MalformedURLException, InterruptedException {
+    @BeforeAll
+    public static void init() {
         dashboardPage = new DashboardPage();
         glassPage = new GlassPage();
         loginPage = new LoginPage();
@@ -26,26 +26,26 @@ public class GlassWorkflowTest {
         loginPage.loggingIn(Util.VALID_USERNAME, Util.VALID_PASSWORD);
         dashboardPage.waitToPresentProfileBtn();
         loginPage.navigate("https://jira-expert.codecool.metastage.net/projects/BET?selectedItem=com.metainf.jira.plugin:glass-project-documentation#/home/issueTypes/10004/transitions");
+        glassPage.clickSkipButton();
+        glassPage.waitTable();
+        tabel = glassPage.returnTable();
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/tableData.csv")
     public void tableValidation(int index, String FromStatus, String TransitionName, String ToStatus, String TransitionType, String TransitionScreen, String Conditions, String Validators, String PostFunctions){
-        glassPage.clickSkipButton();
-       System.out.println(index);
-        System.out.println(glassPage.returnTable());
-        Assertions.assertEquals(FromStatus, glassPage.returnTable().get(index).get(0));
-        Assertions.assertEquals(TransitionName, glassPage.returnTable().get(index).get(1));
-        Assertions.assertEquals(ToStatus, glassPage.returnTable().get(index).get(2));
-        Assertions.assertEquals(TransitionType, glassPage.returnTable().get(index).get(3));
-        Assertions.assertEquals(TransitionScreen, glassPage.returnTable().get(index).get(4));
-        Assertions.assertEquals(Conditions, glassPage.returnTable().get(index).get(5));
-        Assertions.assertEquals(Validators, glassPage.returnTable().get(index).get(6));
-        Assertions.assertEquals(PostFunctions, glassPage.returnTable().get(index).get(7));
+        Assertions.assertEquals(FromStatus, tabel.get(index).get(0));
+        Assertions.assertEquals(TransitionName, tabel.get(index).get(1));
+        Assertions.assertEquals(ToStatus, tabel.get(index).get(2));
+        Assertions.assertEquals(TransitionType, tabel.get(index).get(3));
+        Assertions.assertEquals(TransitionScreen, tabel.get(index).get(4));
+        Assertions.assertEquals(Conditions, tabel.get(index).get(5));
+        Assertions.assertEquals(Validators, tabel.get(index).get(6));
+        Assertions.assertEquals(PostFunctions, tabel.get(index).get(7));
     }
 
-    @AfterEach
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
         loginPage.quit();
     }
 }

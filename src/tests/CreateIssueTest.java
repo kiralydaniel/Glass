b6c6b94cf8fae3260081559383e619a2f4b77e05
SsdHUN <santhasoma1999@gmail.com>
2023-04-13 14:07:58 +0200

    implement successfulCreateIssue

--- src/tests/CreateIssueTest.java
+++ src/tests/CreateIssueTest.java
@@ -1,11 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import pageFactory.BasePage;
-import pageFactory.DashboardPage;
-import pageFactory.LoginPage;
-import pageFactory.Util;
+import pageFactory.*;
 
 import java.net.MalformedURLException;
 
@@ -13,6 +11,8 @@ public class CreateIssueTest extends BasePage {
 
     static LoginPage loginPage;
     static DashboardPage dashboard;
+
+    static IssuePage issuePage;
 
 
     @BeforeEach void init() {
@@ -31,6 +31,23 @@ public class CreateIssueTest extends BasePage {
         dashboard.fillSummaryField(summary);
         dashboard.clickCancelIssueBtn();
     }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/createIssue.csv")
+    public void successfulCreateIssue(String project, String type, String summary) {
+        dashboard.clickCreateBtn();
+        dashboard.fillProjectField(project);
+        dashboard.fillTypeField(type);
+        dashboard.fillSummaryField(summary);
+        dashboard.clickCreateIssueBtn();
+        dashboard.clickCreatedIssueLink();
+        String EXPECTED_TYPE = issuePage.getType();
+        String EXPECTED_SUMMARY = issuePage.getSummary();
+        Assertions.assertEquals(EXPECTED_TYPE, type);
+        Assertions.assertEquals(EXPECTED_SUMMARY, summary);
+        issuePage.deleteIssue();
+        Assertions.assertTrue(issuePage.isDeleteIssueValidate());
+    }
+
     @AfterEach
     public void tearDown(){
         loginPage.quit();
